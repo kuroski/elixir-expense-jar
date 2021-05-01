f04db60cdba84fbd1c621f05f6655ce6bbf97654
Daniel Kuroski <daniel.kuroski@gmail.com>
2021-05-01 17:07:25 +0200

    Update dev database credentials

--- config/dev.exs
+++ config/dev.exs
@@ -3,10 +3,10 @@ use Mix.Config
 # Configure your database
 config :expense_jar, ExpenseJar.Repo,
   username: "postgres",
-  password: "password",
+  password: "postgres",
   database: "expense_jar_dev",
   hostname: "localhost",
-  port: 54320,
+  port: 5432,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
